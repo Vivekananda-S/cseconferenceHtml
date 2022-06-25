ff40f2b430edf48a501ef25f724fdec33a837fd9
Vivekananda-S <75522115+Vivekananda-S@users.noreply.github.com>
2022-06-25 18:45:30 +0530

    heroku

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
     <!-- Local Styles -->
     <link rel="stylesheet" href="Styles/css1.css">
@@ -19,13 +19,10 @@
 </head>
 <body>
     
-<?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/cseconference/Assets/"; include($IPATH."headnavbar.php"); ?>
-
-  </div>
+    <!-- HeadNavBarBootstrap -->
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/cseconference/Assets/"; include($IPATH."headnavbar.php"); ?>
   
     <!-- Toogle Right Canvas  -->
-
-
     <button class="btn btn-danger bttn-custom" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Important Dates</button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
       <div class="offcanvas-header">
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8a8b6891ac035de5839797c023b476e6456d6a3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8a8b6891ac035de5839797c023b476e6456d6a3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v220',
+            'version' => '220.0.0.0',
+            'reference' => 'af0935921c10dc7d2d2f41b06c0a66c89102d8dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
