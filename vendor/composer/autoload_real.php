708110425dca42b8207159a19ae54a14d6ea6430
sricharan2107 <sricharan210701@gmail.com>
2022-06-25 06:18:19 +0530

    added lock

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit680a68a6594d074b24ba5c3192172b44
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit680a68a6594d074b24ba5c3192172b44::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire680a68a6594d074b24ba5c3192172b44($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire680a68a6594d074b24ba5c3192172b44($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
